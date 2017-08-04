c477eebdd8e2785fb8bc7d7a67baf168a7a57f9f
ShivamKumarJha <jha.shivam3@gmail.com>
2017-08-04 13:06:26 +0530

    Use PopupMenu to store duration & time slot.

--- app/src/main/java/com/apptozee/drivingschool/Driver/UpdateCustomer.java
+++ app/src/main/java/com/apptozee/drivingschool/Driver/UpdateCustomer.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
 import android.text.TextUtils;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -38,6 +40,94 @@ public class UpdateCustomer extends AppCompatActivity {
         e4.setText(bundle.getString("cslot"));
 
         b.setText("UPDATE");
+
+        e3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(UpdateCustomer.this, v);
+                //inflating menu from xml resource
+                popup.inflate(R.menu.menu_days);
+                //adding click listener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.days7:
+                                e3.setText("7");
+                                break;
+                            case R.id.days15:
+                                e3.setText("15");
+                                break;
+                            case R.id.days30:
+                                e3.setText("30");
+                                break;
+                            case R.id.days45:
+                                e3.setText("45");
+                                break;
+                        }
+                        return false;
+                    }
+                });
+                //displaying the popup
+                popup.show();
+            }
+        });
+
+        e4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(UpdateCustomer.this, v);
+                //inflating menu from xml resource
+                popup.inflate(R.menu.menu_slot);
+                //adding click listener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.cslot1:
+                                e4.setText(R.string.slot1);
+                                break;
+                            case R.id.cslot2:
+                                e4.setText(R.string.slot2);
+                                break;
+                            case R.id.cslot3:
+                                e4.setText(R.string.slot3);
+                                break;
+                            case R.id.cslot4:
+                                e4.setText(R.string.slot4);
+                                break;
+                            case R.id.cslot5:
+                                e4.setText(R.string.slot5);
+                                break;
+                            case R.id.cslot6:
+                                e4.setText(R.string.slot6);
+                                break;
+                            case R.id.cslot7:
+                                e4.setText(R.string.slot7);
+                                break;
+                            case R.id.cslot8:
+                                e4.setText(R.string.slot8);
+                                break;
+                            case R.id.cslot9:
+                                e4.setText(R.string.slot9);
+                                break;
+                            case R.id.cslot10:
+                                e4.setText(R.string.slot10);
+                                break;
+                            case R.id.cslot11:
+                                e4.setText(R.string.slot11);
+                                break;
+                            case R.id.cslot12:
+                                e4.setText(R.string.slot12);
+                                break;
+                        }
+                        return false;
+                    }
+                });
+                //displaying the popup
+                popup.show();
+            }
+        });
 
         b.setOnClickListener(new View.OnClickListener() {
             @Override
